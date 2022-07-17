493f367f46d151339f2f2bd4c963f627e398954e
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2022-07-17 23:26:14 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -257,3 +257,17 @@
           element.classList.toggle("dark-mode");
       }
   }
+/**
+   * Visitors
+   */
+var counterContainer = document.querySelector(".website-counter");
+var visitCount = localStorage.getItem("page_view");
+
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
